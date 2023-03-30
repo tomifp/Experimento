ae09fb40645e61218d33650f8e6e7cfc6d175d39
Tomas Perez <tomfran000@gmail.com>
2023-03-30 20:14:38 -0300

    primer commit

---
+++ git.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IntentoGit;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Acer
+ */
+public class git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        intento git
+        */
+        
+        System.out.println("Hola mundo");
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingresa tu nombre");
+        String nombre = read.next();
+        System.out.println("Hola " + nombre);
+    }
+    
+}
